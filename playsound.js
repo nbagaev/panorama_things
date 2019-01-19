dd225c76fc3e35d1207408106c403b82bbdc94a9
nbagaev <pomojka@online.de>
2019-01-19 19:24:22 +0100

    add playsound.js

---
+++ playsound.js
@@ -0,0 +1,51 @@
+'use strict';
+
+
+var playBGSoundToggleElement = document.querySelector('#playBGSoundToggle');
+var bgSound = document.querySelector('#bgSound');
+bgSound.volume = 0.5;
+bgSound.loop=true;
+// Set handler for sound play toggle.
+playBGSoundToggleElement.addEventListener('click', playBGSoundToggle);
+
+function playBGSound(scene_id) {	
+	bgSound.src = scene_id + ".mp3"
+	BGSound_play();
+}
+function BGSound_play() {
+	var pl = bgSound.play();
+	if (pl !== undefined) {
+		pl.then(_ => {
+		// play started!
+		showButtonStop();
+		}).catch(error => {
+		// play was prevented.
+		showButtonPlay();
+		});
+	}
+}
+
+function showButtonStop() {
+	if (playBGSoundToggleElement.classList.contains('enabled')) {
+		playBGSoundToggleElement.classList.remove('enabled');
+    } 
+}
+
+function showButtonPlay() {
+	if (!playBGSoundToggleElement.classList.contains('enabled')) {
+		playBGSoundToggleElement.classList.add('enabled');		
+    } 
+}
+  
+function playBGSoundToggle() {
+	if (playBGSoundToggleElement.classList.contains('enabled')) {
+		playBGSoundToggleElement.classList.remove('enabled');
+		BGSound_play();
+    } else {
+		playBGSoundToggleElement.classList.add('enabled');
+		bgSound.pause();
+    }
+}
+
+
+ 
